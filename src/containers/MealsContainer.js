e1065fd5cedd8c6e2bbf9ec9aadbce4c14c47aab
Jake Raffe <jakeraffe24@gmail.com>
2022-03-21 11:38:25 +0000

    MealsPage fetch request

--- src/containers/MealsContainer.js
+++ src/containers/MealsContainer.js
@@ -1,11 +1,15 @@
 import MealCard from "../components/MealCard";
 
-const MealsContainer = () => {
+const MealsContainer = ({meals}) => {
+
+    const mealCards = meals.map(meal => {
+        return <MealCard meal={meal} key={meal.id}/>
+    })
 
     return (
         <>
             <h2>Meal Container</h2>
-            <MealCard/>
+            {mealCards}
         </>
     )
 }
--- src/pages/MealsPage.js
+++ src/pages/MealsPage.js
@@ -1,15 +1,25 @@
 import Header from "../components/Header";
 import SuggestedMealsContainer from "../containers/SuggestedMealsContainer";
 import MealsContainer from "../containers/MealsContainer";
+import { useEffect, useState } from "react";
 
 const MealsPage = () => {
+
+    const [allMeals, setAllMeals] = useState([]);
+
+    const loadAllMeals = () => {fetch("http://localhost:8080/meals")
+            .then(response => response.json())
+            .then(data => setAllMeals(data))
+            .catch(error => console.error(error))   
+}
+    useEffect(loadAllMeals, []);
 
 
     return(
         <>
             <Header/>
             <SuggestedMealsContainer/>
-            <MealsContainer/>
+            <MealsContainer meals={allMeals}/>
         </>
     );
 }
