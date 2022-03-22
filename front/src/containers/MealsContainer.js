4378b3f5cd6df2f41d47ee59e45cc041ddd847b3
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-03-22 13:16:44 +0000

    add cycling sheets of cards

--- front/src/containers/MealsContainer.js
+++ front/src/containers/MealsContainer.js
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
 import MealCard from "../components/MealCard";
 import SearchBar from "../components/SearchBar";
 
 const MealsContainer = ({meals}) => {
 
-    const mealCards = meals.map(meal => {
-        return <MealCard meal={meal} key={meal.id}/>
-    })
+    const cardsPerPage = 4;
+    const lastSheet = Math.ceil(meals.length/cardsPerPage);
+    const [mealCards, setMealCards] = useState([]);
+    const [sheetNo, setSheetNo] = useState(1);
+
+    const updateMealCards = () => {
+        // setMealCards([]);
+        const newMealCards = [];
+        const start = (sheetNo-1) * cardsPerPage;
+        for (let i = start; i < start + cardsPerPage; i++) {
+            if (meals.length > i) {
+                const meal = meals.at(i);
+                newMealCards.push(<MealCard meal={meal} key={meal.id}/>)
+            }
+        }
+        setMealCards(newMealCards);
+    }
+
+    const cycleSheet = (cycleAmount) => {
+        const target = sheetNo + cycleAmount;
+        if (target > 0 && target <= lastSheet) {
+            setSheetNo(target);
+        }
+    }
+
+    useEffect(updateMealCards,[meals, sheetNo]);
 
     return (
         <>
             <h2>Meal Container</h2>
             <SearchBar/>
-            {mealCards}
+            <button onClick={() => cycleSheet(-1)}>Back</button>
+            <span>{sheetNo}/{lastSheet}</span>
+            <button onClick={() => cycleSheet(1)}>Next</button>
+            <div className="cardFlex">
+                {mealCards}
+            </div>
         </>
     )
 }
